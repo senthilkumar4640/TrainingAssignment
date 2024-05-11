4c5cc404e7b5da25c296230f3a3a956b8868ebbd
senthilkumar4640 <senthilkumar.sf4640@gmail.com>
2024-05-11 23:58:23 +0530

    Assembly Reference

---
+++ AssemblyReference/Application/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using CollegeDetails;
+namespace Application;
+class Program 
+{
+    public static void Main(string[] args)
+    {
+        //Default Data Calling
+        Operations.AddDefaultData();
+
+        //Calling Main Menu
+        Operations.MainMenu();
+    }
+}
---
+++ AssemblyReference/CollegeDetails/StudentDetails.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CollegeDetails;
+
+    //Enum Declaration
+    public enum Gender {Select, Male, Female, Transgender}
+    public class StudentDetails
+    {
+        /*
+        a.	StudentID – (AutoGeneration ID – SF3000)
+        b.	StudentName
+        c.	FatherName
+        d.	DOB
+        e.	Gender – Enum (Male, Female, Transgender)
+        f.	Physics
+        g.	Chemistry
+        h.	Maths
+        */
+
+        //Field
+        //Static Field
+        private static int s_studentID = 3000;
+
+        //Properties
+        public string StudentID { get; } //Read Only Property
+        public string StudentName { get; set; }
+        public string FatherName { get; set; }
+        public DateTime DOB { get; set; }
+        public Gender Gender { get; set; }
+        public int PhysicsMark { get; set; }
+        public int ChemistryMark { get; set; }
+        public int MathsMark { get; set; }
+
+        //Constructor
+        public StudentDetails(string studentName, string fatherName, DateTime dob, 
+        Gender gender, int physicsMark, int chemistryMark, int mathsMark )
+        {
+            //Auto Incrementation
+            s_studentID++;
+
+            //Assigning
+            StudentID = "SF" + s_studentID;
+            StudentName = studentName;
+            FatherName = fatherName;
+            DOB = dob;
+            Gender = gender;
+            PhysicsMark = physicsMark;
+            ChemistryMark = chemistryMark;
+            MathsMark = mathsMark;
+
+        }
+
+        //Methods
+        /*
+        Check Eligibility (Method with argument, with return types) – cutOff -75.0
+        Calculate average of 3 marks, if average >= 75.0 return true, else false
+        */
+        public double Average()
+        {
+            int total = PhysicsMark + ChemistryMark + MathsMark;
+            double average = (double)total / 3;
+            return average;
+        }
+
+        public bool CheckEligibility(double cutOff)
+        {
+            if(Average()>=cutOff)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
