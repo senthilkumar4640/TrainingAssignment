b643e513376dfd44138b6603439fa090556be3b1
senthilkumar4640 <senthilkumar.sf4640@gmail.com>
2024-05-11 23:59:29 +0530

    DLL Reference

---
+++ DLLReference/Application/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Application;
+class Program 
+{
+    public static void Main(string[] args)
+    {
+        //Default Data Calling
+        Operations.AddDefaultData();
+
+        //Calling Main Menu
+        Operations.MainMenu();
+    }
+}
