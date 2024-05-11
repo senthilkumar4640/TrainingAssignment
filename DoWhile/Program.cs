657aee16c517935d8030ef3f232cacdc157ecf2e
senthilkumar4640 <senthilkumar.sf4640@gmail.com>
2024-05-12 00:22:59 +0530

    Do While Loop

---
+++ DoWhile/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Xml.XPath;
+namespace Dowhile;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string result;
+        do
+        {
+
+        Console.Write("Enter the number : ");
+        int num = int.Parse(Console.ReadLine());
+
+        if(num % 2 == 0)
+        {
+            Console.WriteLine("Even");
+        }
+        else
+        {
+            Console.WriteLine("Odd");
+        }
+            
+        Console.Write("You need to check another number? - yes/no : ");
+        result = Console.ReadLine();
+
+        while(result != "yes" && result !="no"){
+   
+            Console.Write("Your input is wrong and type a valid input. - yes/no : ");
+            result = Console.ReadLine();
+
+        }
+
+        }while(result=="yes");
+    }
+}
