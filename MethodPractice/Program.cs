1de03013969bee80357a308fc4baf20b8e11c444
senthilkumar4640 <senthilkumar.sf4640@gmail.com>
2024-05-12 00:24:43 +0530

    Method Practice

---
+++ MethodPractice/Program.cs
@@ -0,0 +1,82 @@
+﻿// using System;
+// namespace MethodPractice;
+// class Program
+// {
+//     public static void Main(string[] args)
+//     {
+    //     double multiple = mul(7,8);
+    //     Console.WriteLine(multiple);
+
+    //     add(2,3);
+
+    //     hello();
+
+    //     Console.WriteLine(div(5.5f,7));
+
+    //     Console.WriteLine(sub());
+
+
+    // }
+
+    // public static double mul(double a, double b)//with return and with argument
+    // {
+    //     return a*b;
+    // }
+
+    // public static void add(int a, int b)//without return and with argument
+    // {
+    //     Console.WriteLine(a+b);
+    // }
+
+    // public static float div(float a, float b)//without return and with argument
+    // {
+    //     return a/b;
+    // }
+
+    // public static int sub ()//with return without argument
+    // {
+    //     return 10*8;
+    // }
+
+    // public static void hello()//without return and without argument
+    // {
+    //     Console.WriteLine("Hello");
+    // }
+
+using System;
+
+        public class Program
+        {
+            public static void Main(string[] args)
+            {
+            //         int number = int.Parse(Console.ReadLine());
+            //         int i =1;
+            //         do
+            //         {
+            //         if(i==isArmstrongNum(i))
+            //         {
+            //            Console.WriteLine(i);
+            //         }  
+            //         i++;
+            //         }
+            //         while(i<=number);
+            // }
+            // public static int isArmstrongNum(int num)
+            // {
+            //     int result = 0;
+            //     string S = num.ToString();
+            //     int n = S.Length;
+            //     while(num!=0)
+            //     {
+            //         int digit = num%10;
+            //         result = result + (int)Math.Pow(digit,n);
+            //         num = num/10;
+            //     }
+            //     return result;
+            }
+        }
+            
+
+//     }
+
+// }
