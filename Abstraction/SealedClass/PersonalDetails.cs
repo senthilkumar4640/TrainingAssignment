c1b34c63b1dcfbed90fdbbcb29e7e9745f9bccd5
senthilkumar4640 <senthilkumar.sf4640@gmail.com>
2024-05-11 23:57:36 +0530

    Sealed Class

---
+++ Abstraction/SealedClass/PersonalDetails.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SealedClass
+{
+    public sealed class PersonalDetails : StudentDetails 
+    {
+        //Sealed Class
+    }
+}
