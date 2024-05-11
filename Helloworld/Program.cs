e130aadbbdd4a5765e0126ae7325c8d41187d7a8
senthilkumar4640 <senthilkumar.sf4640@gmail.com>
2024-05-12 00:23:25 +0530

    HelloWorld Program

---
+++ Helloworld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace HelloWorld;//File Scoped Namespace
+/// <summary>
+/// 
+/// </summary>
+    class Program
+    {
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+
