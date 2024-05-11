326a062bf869ee3e40b2117f59970877e9297abd
senthilkumar4640 <senthilkumar.sf4640@gmail.com>
2024-05-12 00:23:40 +0530

    If Condition

---
+++ IfCondition/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace IfCondition;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter the mark : ");
+        double mark = double.Parse(Console.ReadLine());
+     
+        if (mark > 80 && mark <=100)
+        {
+            Console.WriteLine("Grade A");
+        }
+        else if(mark <= 80 && mark >=61)
+        {
+            Console.WriteLine("Grade B");
+        }
+        else if(mark <=60 && mark >=36)
+        {
+            Console.WriteLine("Grade C");
+        }
+        else if(mark <36 && mark>0)
+        {
+            Console.WriteLine("Grade D");
+        }
+        else
+        {
+            Console.WriteLine("Invalid Input");
+
+        }
+    }
+}
