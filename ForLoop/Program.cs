aaf9d2f49dd4e51a1921041a4c1e923c9b366253
senthilkumar4640 <senthilkumar.sf4640@gmail.com>
2024-05-12 00:23:09 +0530

    For Loop

---
+++ ForLoop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography.X509Certificates;
+namespace ForLoop;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter the start number : ");
+        int startNum = int.Parse(Console.ReadLine());
+        Console.Write("Enter the end number : ");
+        int endNum = int.Parse(Console.ReadLine());
+             int result=0;
+
+        for(int i=startNum; i<=endNum; i++)
+        {
+           // int result;
+            result = result +(i*i);
+        }
+        Console.WriteLine(result);
+    }
+
+}
