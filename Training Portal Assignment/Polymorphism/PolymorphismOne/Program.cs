479a047376c610102755c81ff7e5c594c832f9fa
senthilkumar4640 <senthilkumar.sf4640@gmail.com>
2024-05-12 00:12:34 +0530

    Polymorphism

---
+++ Training Portal Assignment/Polymorphism/PolymorphismOne/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+namespace PolymorphismOne;
+class Program 
+{
+    public static void Main(string[] args)
+    {
+       Console.WriteLine(MultiplyMethod(5));
+       Console.WriteLine(MultiplyMethod(5,8));
+       Console.WriteLine(MultiplyMethod(5,8,9));
+       Console.WriteLine(MultiplyMethod(5.1,8));
+       Console.WriteLine(MultiplyMethod(5.1,8.1,9));
+        
+    }
+     //Method 1
+    public static int MultiplyMethod(int n)
+    {
+            return n*n;
+    }
+
+     //Method 2
+     public static int MultiplyMethod(int a, int b)
+     {
+        return a*b;
+     }
+
+      //Method 3
+     public static int MultiplyMethod(int a, int b, int c)
+     {
+        return a*b*c;
+     }
+
+      //Method 4
+     public static double MultiplyMethod(double a, int b)
+     {
+        return a*b;
+     }
+
+      //Method 5
+     public static double MultiplyMethod(double a, double b,int c)
+     {
+        return a*b;
+     }
+
+    
+}
---
+++ Training Portal Assignment/Polymorphism/PolymorphismTwo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace PolymorphismTwo;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(MultiplyMethod(5));
+        Console.WriteLine(MultiplyMethod(5.444444887778888));
+        Console.WriteLine(MultiplyMethod(5.9));
+        Console.WriteLine(MultiplyMethod(589876543456));
+
+    }
+    //Method 1
+    public static int MultiplyMethod(int n)
+    {
+        return n * n;
+    }
+
+    //Method 2
+    public static double MultiplyMethod(double n)
+    {
+        return n * n;
+    }
+
+    //Method 3
+    public static float MultiplyMethod(float n)
+    {
+        return n * n;
+    }
+
+    //Method 1
+    public static long MultiplyMethod(long n)
+    {
+        return n * n;
+    }
+}
