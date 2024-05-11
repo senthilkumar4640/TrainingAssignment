4d7e53a01d4c352fa1beeaff488616b2f1bc8513
senthilkumar4640 <senthilkumar.sf4640@gmail.com>
2024-05-12 00:25:05 +0530

    String Manipulation

---
+++ StringManipulation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace StringManipulation;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter the Main String : ");
+        string mainString = Console.ReadLine();
+        Console.Write("Enter the Sub String : ");
+        string subString = Console.ReadLine();
+        string[] res= mainString.Split(subString);
+        int count = res.Length-1;
+        Console.WriteLine(count);
+    }
+}
