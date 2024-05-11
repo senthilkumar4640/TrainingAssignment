c6e4a1d1be757ca956b4512f18f4166875af79d4
senthilkumar4640 <senthilkumar.sf4640@gmail.com>
2024-05-12 00:25:14 +0530

    Switch Case

---
+++ SwitchCase/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+namespace SwitchCase;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter the num 1 : ");
+        double num1 = double.Parse(Console.ReadLine());
+        Console.Write("Enter the operand to perform (+,-,*,/,%)");
+        string operand = Console.ReadLine();
+        Console.WriteLine("Enter the num 2 : ");
+        double num2 = double.Parse(Console.ReadLine());
+
+        switch(operand)
+        {
+            case "+":
+            {
+                Console.WriteLine($"{num1} + {num2} = {num1+num2}");
+                break;
+            }
+            case "-":
+            {
+                Console.WriteLine($"{num1} - {num2} = {num1-num2}");
+                break;
+            }
+            case "*":
+            {
+                Console.WriteLine($"{num1} * {num2} = {num1*num2}");
+                break;
+            }
+            case "/":
+            {
+                Console.WriteLine($"{num1} / {num2} = {num1/num2}");
+                break;
+            }
+            case "%":
+            {
+                Console.WriteLine($"{num1} % {num2} = {num1%num2}");
+                break;
+            }
+            default:
+            {
+                Console.WriteLine("Invalid Input");
+                break;
+            }
+
+         
+            }
+
+        }
+
+        
+    }
